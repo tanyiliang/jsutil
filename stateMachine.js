79ca27434eee1e8c15bdd5028fb49924bedfb788
tan.yiliang <tan.yiliang@zte.com.cn>
2015-08-18 16:43:41 +0800

    状态机

---
+++ stateMachine.js
@@ -0,0 +1,29 @@
+//扩展jquery现有的DOM外部事件的支持
+var Events = {
+  bind: function(){
+    if(!this.o) this.o  = $({});
+    this.o.bind.apply(this.o,arguments);  
+  },
+  trigger:function(){
+    if(!this.o) this.o = $({});
+    this.o.trigger.apply(this.o,arguments)
+  }
+}
+
+var StateMachine = function(){};
+StateMachine.fn = StateMachine.prototype;
+
+$.extend(StateMachine.fn,Events);
+
+StateMachine.fn.add = function(controller){
+  this.bind("change",function(e,current){
+    if(controller == current)
+      controller.activate();
+    else 
+      controller.deactivate();
+  });
+
+  controller.active = $.proxy(function(){
+    this.trigger("change",controller);
+  },this);
+}
